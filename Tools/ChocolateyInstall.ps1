27dd88d930f0b1ddbf0ad2aa9fa84f970a1805f3
Matt Fellows <matt.fellows@onegeek.com.au>
2015-01-23 17:51:22 +1100

    Packer should be installed in %APPDATA$\packer.d

--- Tools/ChocolateyInstall.ps1
+++ Tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "PackerCommunityPlugins"
 $url64 = "https://github.com/packer-community/packer-windows-plugins/releases/download/pre-release/windows_amd64.zip"
 $url = "https://github.com/packer-community/packer-windows-plugins/releases/download/pre-release/windows_386.zip"
-$unzipLocation = "${env:chocolateyPackageFolder}"
+$unzipLocation = "${env:APPDATA}\packer.d"
 
 Install-ChocolateyZipPackage $packageName $url $unzipLocation $url64
