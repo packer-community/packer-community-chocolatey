12cb38b8978bd8ef5856cb6e812d3239ec224820
Matt Fellows <matt.fellows@onegeek.com.au>
2015-01-23 23:55:56 +1100

    Docu on packer.io website is incorrect, plugins should be placed in   on Windows platforms

--- Tools/ChocolateyInstall.ps1
+++ Tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "PackerCommunityPlugins"
 $url64 = "https://github.com/packer-community/packer-windows-plugins/releases/download/pre-release/windows_amd64.zip"
 $url = "https://github.com/packer-community/packer-windows-plugins/releases/download/pre-release/windows_386.zip"
-$unzipLocation = "${env:APPDATA}\packer.d"
+$unzipLocation = "${env:APPDATA}\packer.d\plugins"
 
 Install-ChocolateyZipPackage $packageName $url $unzipLocation $url64
