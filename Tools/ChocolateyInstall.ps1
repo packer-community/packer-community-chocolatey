918c0e2f4d51d3833a1842585f64d1b66ea4f469
Matt Fellows <matt.fellows@onegeek.com.au>
2015-03-23 12:41:17 +1100

    Updated to v1.0.0-RC

--- Tools/ChocolateyInstall.ps1
+++ Tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "PackerCommunityPlugins"
-$url64 = "https://github.com/packer-community/packer-windows-plugins/releases/download/pre-release/windows_amd64.zip"
-$url = "https://github.com/packer-community/packer-windows-plugins/releases/download/pre-release/windows_386.zip"
+$url64 = "https://github.com/packer-community/packer-windows-plugins/releases/download/v1.0.0-rc/windows_amd64.zip"
+$url = "https://github.com/packer-community/packer-windows-plugins/releases/download/v1.0.0-rc/windows_386.zip"
 $unzipLocation = "${env:APPDATA}\packer.d\plugins"
 
 Install-ChocolateyZipPackage $packageName $url $unzipLocation $url64
